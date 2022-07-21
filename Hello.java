0be12fc3919072ff76eed0bf5368ec5b94cb42bd
harshitjain0786 <109657173+harshitjain0786@users.noreply.github.com>
2022-07-21 22:37:45 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+//this is the program to print hello message
+
+
+class Hello
+{
+	public static void main(String args[])
+	
+	{
+	
+		System.out.println("hello guys this is my first lab");
+	
+	
+}
+}
+/*
+void runvehicle()
+{
+	jhcjh
+}
+*/
---
+++ Increment.java
@@ -0,0 +1,13 @@
+class Increment
+{
+	public static void main(String args[])
+	{
+		int x=1;
+		int y=x++;
+		int z=++x;
+		System.out.println(" value of y is" +y);
+		System.out.println(" value of z is" +z);
+	}
+
+}
+//program to find sum , difference ,average and product of two numbrrs
