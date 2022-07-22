401539e8ba1f62c861c67f13df26257cc17fc9ee
harshitjain0786 <109657173+harshitjain0786@users.noreply.github.com>
2022-07-22 14:03:26 +0530

    Add files via upload

---
+++ CelToFahren.java
@@ -0,0 +1,15 @@
+ //this program of cel. to fahrenheit
+ 
+ class CelToFahren
+{
+	public static void main(String args[])
+	{
+		float celcius,fahrenheit;
+double CelToFahren;
+		celcius=47;
+		CelToFahren=celcius*1.8+32;
+		{
+			System.out.println("celcius to fahrenheit is =" +CelToFahren);
+		}
+	}
+}
