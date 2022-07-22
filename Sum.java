763636a4d72d7f355917cf6383fbcdb296371736
harshitjain0786 <109657173+harshitjain0786@users.noreply.github.com>
2022-07-22 17:29:14 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -4,14 +4,16 @@ class Sum
 public static void main(String args[])
 
 {
-int a=10,b=45;
+int a=50,b=45;
 int difference=b-a;
 int sum=a+b;
+int product=a*b;
 double average=sum/2;
 {
-	System.out.println("the sum of a and b is =" +sum);
-	System.out.println("the average of a and b is="+average);
-	System.out.println("the difference of a and b="+difference);
+	System.out.println("the sum of a and b is = " +sum);
+	System.out.println("the average of a and b is= "+average);
+	System.out.println("the difference of a and b= "+difference);
+        System.out.println("the product of a and b is = "+product);
 }
 
 }
