7b4c94d3b6402e1634158232b16809ea317d551b
harshitjain0786 <109657173+harshitjain0786@users.noreply.github.com>
2022-07-22 19:04:36 -0700

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -9,12 +9,12 @@ int difference=b-a;
 int sum=a+b;
 int product=a*b;
 double average=sum/2;
-{
+
 	System.out.println("the sum of a and b is = " +sum);
 	System.out.println("the average of a and b is= "+average);
 	System.out.println("the difference of a and b= "+difference);
         System.out.println("the product of a and b is = "+product);
-}
+
 
 }
 }
