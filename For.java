4c74a5a1ea61ce22df6ffdf3dced1a7eab48bed4
harshitjain0786 <109657173+harshitjain0786@users.noreply.github.com>
2022-07-29 14:54:18 +0530

    Add files via upload

---
+++ For.java
@@ -0,0 +1,10 @@
+class For
+{
+	public static void main(String[] args) {
+		int i=10;
+		for(i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
---
+++ Forloop.java
@@ -0,0 +1,14 @@
+class Forloop
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=5;i++)
+		S{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.println(" * ");
+			}
+			System.out.println();
+		}
+	}
+}
